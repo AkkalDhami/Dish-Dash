9be2d7ce2e5373a32e98e13be783ca099f3f9a9d
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-21 21:03:03 +0545

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/**/*.html", "./css/**/*.css", "./js/**/*.js", "./index.html"],
+  content: [
+    "./html/**/*.html",
+    "./css/**/*.css",
+    "./js/**/*.js",
+    "./index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#0d6efd",
+        primaryHover: "#3284e8",
+        secondary: "#eb5f2f",
+        secondaryHover: "#ff6b38",
+        card: "#f6f6f6",
+        cardHover: "#ffffff",
+      },
+      colors: {
+        primary: "#0d6efd",
+        secondary: "#eb5f2f",
+        textColor: "#000000",
+        textFlatColor: "#727374",
+      },
+    },
   },
   plugins: [],
 };
