c79216e35c2271f16a5ec81c884037718fc49910
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-21 21:06:23 +0545

    update card container

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,11 @@ let menu_container = document.querySelector("#menu_container");
 let item_container = document.querySelector("#item_container");
 
 async function fetchAPI() {
-  let res = await fetch('../assets/json/api.json');
+  let res = await fetch("../assets/json/api.json");
   let data = await res.json();
 
   localStorage.setItem("menuList", JSON.stringify(data.menu_list));
   localStorage.setItem("FoodList", JSON.stringify(data.food_list));
-
 }
 
 fetchAPI();
@@ -15,13 +14,12 @@ fetchAPI();
 function displayMenu() {
   let menu_list = JSON.parse(localStorage.getItem("menuList"));
   let food_list = JSON.parse(localStorage.getItem("FoodList"));
-  console.log(food_list)
   menu_list.forEach((menu, ind) => {
     let { menu_image, menu_name } = menu;
 
     menu_container.innerHTML += `
           <div
-            class="card flex flex-col items-center space-y-3 group flex-grow cursor-pointer"
+            class="foodMenu flex flex-col items-center space-y-3 group flex-grow cursor-pointer"
             data-menu="${menu_name}"
           >
             <div
@@ -39,15 +37,16 @@ function displayMenu() {
               ${menu_name}
             </h3>
           </div>
-        `
+        `;
   });
 
   food_list.forEach((food, ind) => {
     let { _id, category, description, image, name, price, rating } = food;
     item_container.innerHTML += `
-     <div data-category="${category}"
+   <div
+            data-category="${category}"
             id="${_id}"
-            class="card bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden"
+            class="foodItem bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden"
           >
             <div
               class="img max-w-[280px] max-h-[280px] rounded-md overflow-hidden"
@@ -60,9 +59,7 @@ function displayMenu() {
             </div>
             <div class="flex mt-3 flex-col">
               <h3 class="text-[18px] font-semibold">${name}</h3>
-              <h4 class="text-[15px]">
-               ${description}
-              </h4>
+              <h4 class="text-[15px]">${description}</h4>
               <div class="flex mt-2 w-full justify-between">
                 <div class="flex gap-4 items-center">
                   <p class="text-[18px] font-[500]">$${price}</p>
@@ -76,13 +73,26 @@ function displayMenu() {
             <div
               class="mt-2 flex flex-col space-y-3 justify-between w-full items-center"
             >
-              <button
-                class="px-4 flex items-center gap-4 py-2 bg-[#ff6b38] hover:bg-[#eb5f2f] rounded-sm text-white text-[16px] sm:text-[16px] duration-300 w-full justify-center"
-              >
-                Add to Cart <i class="ri-shopping-bag-line"></i>
-              </button>
-              <button
-                class="px-4 flex items-center gap-4 py-2 duration-300 bg-[#3284e8] hover:bg-[#0d6efd] rounded-sm text-white text-[16px] sm:text-[16px] w-full justify-center"
+              <div class="flex items-center justify-between w-full">
+                <button
+                  class="px-4 flex items-center gap-4 py-2 bg-[#ff6b38] hover:bg-[#eb5f2f] rounded-sm text-white text-[16px] sm:text-[16px] duration-300 justify-center"
+                >
+                  Add to Cart <i class="ri-shopping-bag-line"></i>
+                </button>
+                <button 
+                  class="decreaseQuantity text-black bg-[#f6f6f6] text-[18px] px-2 py-1 rounded-sm"
+                >
+                  <i class="decreaseQuantity ri-subtract-line"></i>
+                </button>
+                <p  class="foodQuantity text-[15px] text-black font-[400]">1</p>
+                <button
+                  class="increaseQuantity px-2 bg-[#f6f6f6] text-black text-[16px] py-1 rounded-sm"
+                >
+                  <i class="increaseQuantity ri-add-large-fill"></i>
+                </button>
+              </div>
+              <button 
+                class=" px-4 flex items-center gap-4 py-2 duration-300 bg-[#3284e8] hover:bg-[#0d6efd] rounded-sm text-white text-[16px] sm:text-[16px] w-full justify-center"
               >
                 Buy Now <i class="ri-restaurant-fill"></i>
               </button>
@@ -91,15 +101,15 @@ function displayMenu() {
     `;
   });
 
-  let ratedFoods = food_list.filter(food => food.rating >= 4.6);
+  let ratedFoods = food_list.filter((food) => food.rating >= 4.6);
   let rated_foods_container = document.querySelector("#rated_foods_container");
 
   ratedFoods.forEach((item, ind) => {
     let { _id, category, description, image, name, price, rating } = item;
     rated_foods_container.innerHTML += `
-     <div data-category="${category}"
-            data-id="${_id}"
-            class="card bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden"
+       <div data-category="${category}"
+            id="${_id}"
+            class="foodItem bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden"
           >
             <div
               class="img max-w-[280px] max-h-[280px] rounded-md overflow-hidden"
@@ -112,9 +122,7 @@ function displayMenu() {
             </div>
             <div class="flex mt-3 flex-col">
               <h3 class="text-[18px] font-semibold">${name}</h3>
-              <h4 class="text-[15px]">
-               ${description}
-              </h4>
+              <h4 class="text-[15px]">${description}</h4>
               <div class="flex mt-2 w-full justify-between">
                 <div class="flex gap-4 items-center">
                   <p class="text-[18px] font-[500]">$${price}</p>
@@ -128,22 +136,40 @@ function displayMenu() {
             <div
               class="mt-2 flex flex-col space-y-3 justify-between w-full items-center"
             >
+              <div class="flex items-center justify-between w-full">
+                <button 
+                  class="px-4 flex items-center gap-4 py-2 bg-[#ff6b38] hover:bg-[#eb5f2f] rounded-sm text-white text-[16px] sm:text-[16px] duration-300 justify-center"
+                >
+                  Add to Cart <i class="ri-shopping-bag-line"></i>
+                </button>
+                <button
+                  class="decreaseQuantity text-black bg-[#f6f6f6] text-[16px] px-2 py-1 rounded-sm"
+                >
+                  <i class="decreaseQuantity ri-subtract-line"></i>
+                </button>
+                <p class="foodQuantity text-[15px] text-black font-[400]">1</p>
+                <button 
+                  class="increaseQuantity px-2 bg-[#f6f6f6] text-black text-[16px] py-1 rounded-sm"
+                >
+                  <i class="increaseQuantity ri-add-large-fill"></i>
+                </button>
+              </div>
               <button
-                class="px-4 flex items-center gap-4 py-2 bg-[#ff6b38] hover:bg-[#eb5f2f] rounded-sm text-white text-[16px] sm:text-[16px] duration-300 w-full justify-center"
-              >
-                Add to Cart <i class="ri-shopping-bag-line"></i>
-              </button>
-              <button
-                class="px-4 flex items-center gap-4 py-2 duration-300 bg-[#3284e8] hover:bg-[#0d6efd] rounded-sm text-white text-[16px] sm:text-[16px] w-full justify-center"
+                class=" px-4 flex items-center gap-4 py-2 duration-300 bg-[#3284e8] hover:bg-[#0d6efd] rounded-sm text-white text-[16px] sm:text-[16px] w-full justify-center"
               >
                 Buy Now <i class="ri-restaurant-fill"></i>
               </button>
             </div>
           </div>
-    `
-  })
+    `;
+  });
 
-
+  let menuItems = document.querySelectorAll(".foodMenu");
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      let menu = item.getAttribute("data-menu");
+      window.location.href = `../html/filterFood.html?menu=${menu}`;
+    });
+  });
 }
-displayMenu()
-
+displayMenu();
