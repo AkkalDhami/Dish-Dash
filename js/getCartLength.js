18c9197d82ce72db62653e8d11c5544c4285d287
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-21 21:05:10 +0545

    add get cart total quantity

---
+++ js/getCartLength.js
@@ -0,0 +1,9 @@
+export function getCartLength() {
+    let cart = JSON.parse(localStorage.getItem("myCart")) || [];
+    let cartItems = document.querySelectorAll(".cartItems");
+    let cartLength = cart.reduce((total, product) => total + product.quantity, 0);
+    cartItems.forEach((item) => {
+        item.innerHTML = cartLength
+    })
+
+}
