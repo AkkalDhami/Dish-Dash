00f140c6fc2bb0a753e03f87aa25e8d7cc85cbb8
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-22 21:39:11 +0545

    update signup js

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,4 @@
+import successToast from "../assets/utility/successToast.js";
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("signupForm");
     const signupEmail = document.getElementById("signupEmail");
@@ -75,61 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         if (valid) {
-            toastNotification(
-                `ri-checkbox-circle-fill`,
-                "Form submitted Successfully",
-                "success"
-            );
+            successToast("Signup Successful!");
             clearInput();
 
         }
     });
 
 
-    function toastNotification(icon, message, type) {
-        const toastContainer = document.createElement("div");
-        toastContainer.classList.add("toastBox", type);
-        toastContainer.innerHTML = `
-        <div class="flex items-center space-x-2">
-          <i class="${icon} text-${type} text-[24px]"></i>
-          <div class="ms-3 text-[16px] text-gray-800 font-normal">
-           ${message}
-          </div>
-        </div>
-        <button
-          type="button"
-          class="ml-4 fixed right-3 top-1/2 -translate-y-1/2 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
-          data-dismiss-target="#toastNotify"
-          aria-label="Close"
-        >
-          <span class="sr-only">Close</span>
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
-        </button>
-        <div
-          class="toastLine after:absolute after:left-0 after:bottom-0 after:w-[0%] after:h-[4px] after:bg-[#0d6efd]"
-        ></div>
-      </div>
-        `;
-        document.querySelector(".toastNotify").appendChild(toastContainer);
 
-        setTimeout(() => {
-            toastContainer.classList.add("hidden");
-        }, 3000);
-    }
 
     function togglePasswordVisibility(inputId, iconId) {
         const passwordInput = document.querySelector(
