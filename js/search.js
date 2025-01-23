b1b3d7977c0937721dbb5a3a551294576b576870
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-23 20:04:32 +0545

    update search js

--- js/search.js
+++ js/search.js
@@ -1,22 +1,22 @@
 let searchButton = document.getElementById('searchButton');
 let searchBottomButton = document.getElementById('searchBottomButton');
 let searchResultsContainer = document.getElementById('searchResultsContainer');
-let overlay = document?.querySelector(".overlay");
+let overlay1 = document?.querySelector(".overlay");
 
 searchBottomButton.addEventListener('click', () => {
-    overlay.classList.remove('overlayActive');
+    overlay1.classList.remove('overlayActive');
     searchResultsContainer.classList.add("top-1/2");
     searchResultsContainer.classList.remove("-top-1/2");
 })
 
 searchButton.addEventListener('click', () => {
-    overlay.classList.remove('overlayActive');
+    overlay1.classList.remove('overlayActive');
     searchResultsContainer.classList.add("top-1/2");
     searchResultsContainer.classList.remove("-top-1/2");
 })
 
-overlay.addEventListener('click', () => {
-    overlay.classList.add('overlayActive');
+overlay1.addEventListener('click', () => {
+    overlay1.classList.add('overlayActive');
     searchResultsContainer.classList.remove("top-1/2");
     searchResultsContainer.classList.add("-top-1/2");
 })
