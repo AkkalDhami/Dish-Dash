382994cdc858ad30c2fc4bd7235a514aeaee0f85
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-24 20:27:23 +0545

    update login js

---
+++ js/loginPage.js
@@ -0,0 +1,31 @@
+import { showToast } from '../assets/utility/showToast.js';
+
+function togglePassword() {
+    const passwordInput = document.getElementById("login-Password");
+    const toggleIcon = document.getElementById("password-toggle-icon");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    }
+}
+
+document.querySelector("#password-toggle-icon").addEventListener("click", togglePassword);
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", validateForm);
+function validateForm(event) {
+    event.preventDefault();
+    const email = document.getElementById("login-Email").value;
+    const password = document.getElementById("login-Password").value;
+
+    if (email === "admin@admin.com" && password === "admin123") {
+        showToast("Login successful", "success");
+    } else {
+        showToast("Invalid email or password try again or sign up", "error");
+    }
+}
