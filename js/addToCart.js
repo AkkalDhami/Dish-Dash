cfd1ad83c751319e33786d01b0fcc29b22b8cd48
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-29 23:24:54 +0545

    update add to cart js page

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,62 +1,30 @@
 import { getCartLength } from "./getCartLength.js";
 import { showToast } from "../assets/utility/showToast.js";
+import { addToCart } from "../assets/utility/addToCart.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+    getCartLength();
 
-  const getCart = () => JSON.parse(localStorage.getItem("myCart")) || [];
-  const updateCart = (cart) => localStorage.setItem("myCart", JSON.stringify(cart));
-  const getFoodList = () => JSON.parse(localStorage.getItem("FoodList")) || [];
-
-  getCartLength();
-
-  const addToCart = () => {
     const foodItems = document.querySelectorAll(".foodItem");
-    const FoodList = getFoodList();
-
-    if (!FoodList.length) {
-      showToast("FoodList not found in localStorage!", "error");
-      return;
-    }
-
     foodItems.forEach((item) => {
-      const id = item.id;
-      if (!id) {
-        console.error("ID not found for food item!");
-        return;
-      }
-
-      item.addEventListener("click", (e) => {
-        const cart = getCart();
-        let quantity = 1;
-
-
-        if (e.target.classList.contains("addToCart")) {
-
-          const productInCart = cart.find((product) => product._id === id);
-
-          showToast("Product added to your cart!", "success");
-          if (productInCart) {
-            productInCart.quantity += quantity;
-          } else {
-            const productToAdd = FoodList.find((product) => product._id === id);
-            if (productToAdd) {
-              cart.push({ ...productToAdd, quantity });
-            } else {
-
-              console.error("Product not found in FoodList!");
-              return;
+        item.addEventListener("click", (e) => {
+            const id = item.id;
+            console.log(item)
+            console.log(id)
+            if (!id) {
+                console.error("ID not found for food item!");
+                return;
             }
-          }
-
-          updateCart(cart);
-          getCartLength();
-          console.log("Cart updated:", cart);
-        }
-
-
-      });
+            if (e.target.classList.contains("addToCart")) {
+                let quantity = 1;
+                addToCart(id, quantity);
+            }
+        });
     });
-  };
 
-  addToCart();
+    // addToCartBtn.addEventListener("click", () => {
+    //   const quantity = parseInt(quantityInput.value, 10);
+    //   const productId = product._id;
+    //   addToCart(productId, quantity);
+    // });
 });
