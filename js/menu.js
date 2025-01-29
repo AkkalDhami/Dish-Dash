f06f6c3d43d2781b2fdd6c58d7b4aa49aaab6e27
AkkalDhami <dhamiakkal21@gmail.com>
2025-01-29 23:25:25 +0545

    update root js

--- js/menu.js
+++ js/menu.js
@@ -42,9 +42,10 @@ function displayMenu() {
 
   food_list.forEach((food, ind) => {
     let { _id, category, description, image, name, selling_price, original_price, rating } = food;
+    let discount = Math.floor((original_price - selling_price) / original_price * 100);
+
     item_container.innerHTML += `
-  <div
-            data-category="${category}"
+  <div data-category="${category}"
             id="${_id}"
             class="foodItem bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden"
           >
@@ -58,7 +59,10 @@ function displayMenu() {
               />
             </div>
             <div class="flex mt-3 flex-col">
-              <h3 class="text-[18px] font-semibold">${name}</h3>
+             <div class="flex justify-between my-1 items-center">
+               <h3 class="text-[18px] group-hover:text-[#0d6efd] duration-200 font-semibold">${name}</h3>
+               <span class="text-[#fff] px-3 py-[2px] rounded-full bg-[#ff6b38] sm:text-[14px] text-[12px] font-[500]">${discount}% off</span>
+              </div>
               <h4 class="text-[15px]">${description}</h4>
               <div class="flex mt-2 w-full justify-between">
                 <div class="flex gap-4 items-center">
@@ -76,7 +80,7 @@ function displayMenu() {
               <button
                 class="addToCart px-4 flex items-center gap-4 py-[12px] bg-[#0d6efd] hover:bg-[#3284e8] rounded-full text-white text-[16px] sm:text-[16px] duration-300 justify-center w-full"
               >
-                Add to Cart <i class="ri-shopping-bag-line"></i>
+                Add to Cart <i class="ri-shopping-bag-line addToCart"></i>
               </button>
               <button onclick="window.location.href='../html/foodDetails.html?id=${_id}&category=${category}'"
                 class="viewMore px-4 flex items-center gap-4 py-[10px] border-2 border-[#eb5f2f] hover:bg-[#ff6b38] text-gray-800 rounded-full hover:text-white text-[16px] group/viewMore sm:text-[16px] duration-300 justify-center w-full"
@@ -95,7 +99,7 @@ function displayMenu() {
   menuItems.forEach((item) => {
     item.addEventListener("click", () => {
       let menu = item.getAttribute("data-menu");
-      window.location.href = `../html/filterFood.html?menu=${menu}`;
+      window.location.href = `../html/filterFood.html?menu=${menu.toUpperCase()}`;
     });
   });
 }
