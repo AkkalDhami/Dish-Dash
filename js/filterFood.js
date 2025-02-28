9beb14e40a92d270ed39353a144e409b3b7ab6f1
AkkalDhami <dhamiakkal21@gmail.com>
2025-02-28 14:14:24 +0545

    update toast notification

--- js/filterFood.js
+++ js/filterFood.js
@@ -15,19 +15,26 @@ function filterFood() {
     foodMenuContainer.innerHTML += `
      <div data-category="${category}"
             id="${_id}"
-            class="foodItem bg-[#f6f6f6] p-5 rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden relative"
+            class="foodItem bg-[#f6f6f6]  rounded-md hover:shadow-lg hover:bg-[#ffffff] duration-300 w-[300px] group overflow-hidden relative"
           >
-          <i title="Add to wishlist" class="addToWishList ri-heart-line z-30 duration-500 w-[30px] h-[30px] flex items-center justify-center p-5 cursor-pointer hover:bg-[#ff6b38] bg-white rounded-full hover:text-white absolute top-5 opacity-0 group-hover:opacity-100 -right-80 group-hover:right-6 text-2xl text-[#ff6b38]"></i>
-            <div
-              class="img max-w-[280px] max-h-[280px] rounded-md overflow-hidden"
-            >
+          <div class="absolute top-5 opacity-100 group-hover:opacity-100 right-3  flex space-y-2 duration-300 flex-col">
+            
+            <i title="Add to wishlist" class="addToWishList ri-heart-line z-30 duration-500 w-[30px] h-[30px] flex items-center justify-center p-5 cursor-pointer shadow-[0px_0px_16px_3px_#a19393] hover:bg-[#ff6b38] bg-white rounded-full hover:text-white  text-2xl text-[#ff6b38]"></i>
+            
+            <i title="View Details" onclick="window.location.href='../html/foodDetails.html?id=${_id}&category=${category}'" class="ri-eye-line viewMore z-30 w-[30px] h-[30px] flex items-center justify-center p-5 cursor-pointer hover:bg-[#0d6efd] bg-white rounded-full hover:text-white duration-300 shadow-[0px_0px_16px_3px_#a19393] text-2xl text-[#3284e8]"></i>
+          </div>
+
+            <div onclick="window.location.href='../html/foodDetails.html?id=${_id}&category=${category}'"
+              class="img max-h-[280px] cursor-pointer w-full overflow-hidden"
+            >  
               <img
                 src="${image}"
                 alt="food-${ind} image"
                 class="w-full h-full object-cover object-center scale-100 group-hover:scale-110 duration-300 transition-all"
               />
             </div>
-            <div class="flex mt-3 flex-col">
+
+            <div class="flex p-[8px_12px] mt-3 flex-col">
              <div class="flex justify-between my-1 items-center">
                <h3 class="text-[18px] group-hover:text-[#0d6efd] duration-200 font-semibold">${name}</h3>
                <span class="text-[#fff] px-3 py-[2px] rounded-full bg-[#ff6b38] sm:text-[14px] text-[12px] font-[500]">${discount}% off</span>
@@ -42,22 +49,10 @@ function filterFood() {
                 </div>
                 <h3><i class="ri-star-fill text-[#ff6b38]"></i> | ${rating}</h3>
               </div>
-            </div>
-            <div
-              class="mt-2 flex flex-col space-y-3 justify-between w-full items-center"
-            >
               <button
-                class="addToCart px-4 flex items-center gap-4 py-[12px] bg-[#0d6efd] hover:bg-[#3284e8] rounded-full text-white text-[16px] sm:text-[16px] duration-300 justify-center w-full"
+                class="addToCart mt-3 mb-2 px-4 flex items-center gap-4 py-[12px] bg-[#0d6efd] hover:bg-[#3284e8] rounded-full text-white text-[16px] sm:text-[16px] duration-300 justify-center w-full"
               >
                 Add to Cart <i class="ri-shopping-bag-line addToCart"></i>
-              </button>
-              <button onclick="window.location.href='../html/foodDetails.html?id=${_id}&category=${category}'"
-                class="viewMore px-4 flex items-center gap-4 py-[10px] border-2 border-[#eb5f2f] hover:bg-[#ff6b38] text-gray-800 rounded-full hover:text-white text-[16px] group/viewMore sm:text-[16px] duration-300 justify-center w-full"
-              >
-                View More
-                <i
-                  class="ri-arrow-right-line group-hover/viewMore:translate-x-2 duration-100"
-                ></i>
               </button>
             </div>
           </div>
