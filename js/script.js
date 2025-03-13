813dd244ceba94eaf13b3411ae311affb96f93f8
AkkalDhami <dhamiakkal21@gmail.com>
2025-03-13 10:25:44 +0545

    remove link

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-// Get all navigation links
-const navLinks = document.querySelectorAll("[data-nav-link]");
-// const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
-function setActiveLink(clickedLink) {
-    navLinks.forEach((link) => {
-        link.classList.remove("active");
-    });
-
-    clickedLink.classList.add("active");
-}
-
-navLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        setActiveLink(link);
-
-        localStorage.setItem("activeLink", link.textContent.trim());
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    const savedActiveLink = localStorage.getItem("activeLink");
-    if (savedActiveLink) {
-        const link = Array.from(navLinks).find(
-            (link) => link.textContent.trim() === savedActiveLink
-        );
-        if (link) {
-            setActiveLink(link);
-        }
-    } else {
-        setActiveLink(navLinks[0]);
-    }
-});
 
 document.addEventListener("DOMContentLoaded", function () {
     var menuButton = document.getElementById("menu-button");
